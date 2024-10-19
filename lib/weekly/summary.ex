3f750cc7a94f8b44431e90841dbd8b2e1d555c01
Danny Rosenblatt <dgrosenblatt@gmail.com>
2024-10-19 16:09:32 -0400

    Tweak open ai prompt

--- lib/weekly/summary.ex
+++ lib/weekly/summary.ex
@@ -30,10 +30,14 @@ defmodule Weekly.Summary do
         role: "user",
         content: "I would like you to create a summary of a few emails.
       At the beginning have a special section that lists out any important dates and required action items that are mentioned in the emails.
-      The tone of the summary should be an old jewish man using many yiddish and hebrew expressions and also upbeat and fun.
+      The tone of the summary should be an old jewish man using many yiddish and hebrew expressions.
+      Each paragraph should begin with an emoji relevant to the content.
       The summary should be readable in two minutes or fewer. Err on the side of brevity.
-      The summary should start with an old-timey extremely sensationalist newspaper headline.
-      The emails will be separated with new lines and stars like this- \n\n\n**********\n\n\n ."
+      The summary should start with an alliterative headline.
+      The emails will be separated with new lines and stars like this- \n\n\n**********\n\n\n .
+      After the summary, determine this week's Torah portion, for today's date: in YYYY-MM-DD format: #{Date.utc_today() |> Date.to_string()}
+      Then write and include a limerick based on this week's Torah portion and label it as such.
+      "
       },
       %{
         role: "user",
